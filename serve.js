ab034aab5db2270f1592e1fc0302060f7db680ab
Montana Rowe <calcnerd256@calcnerd256.homeip.net>
2012-03-04 20:49:59 -0600

    coerced port to a number and imported server module correctly

--- serve.js
+++ serve.js
@@ -1,6 +1,6 @@
-this.server = new require("./server").Server();
+this.server = new (require("./server").Server)();
 var port = 15213;
-if(process.argv[2] < 65536)
+if(+(process.argv[2]) < 65536)
  port = process.argv[2];
 this.server.setPort(port);
 this.server.init(
