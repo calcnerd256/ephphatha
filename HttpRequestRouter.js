ce4f69d5902e7a37cb3bca1d23c258e0a0b678da
calcnerd256 <calcnerd256@gmail.com>
2013-01-08 01:10:28 +0000

    functor class for callable-like function-like objects

--- HttpRequestRouter.js
+++ HttpRequestRouter.js
@@ -1,4 +1,44 @@
+var util = require("util");
+
+function coerceToFunction(fn, noisy){
+ if("function" == typeof fn) return fn;
+ var fail = function fail(){
+  if(noisy)
+   throw new Error(
+    [
+     "not a function",
+     fn,
+     "and noisy",
+     noisy,
+     this
+    ]
+   );
+  return function nop(){}
+ }.bind(this);
+ if(!fn) return fail();
+ if("object" != typeof fn) return fail();
+ if("toFunction" in fn) return fn.toFunction();
+ return fail();
+}
+
+function Functor(fn){
+ this["function"] = coerceToFunction(fn, true);
+}
+Functor.prototype.toFunction = function toFunction(){
+ return this["function"].bind(this);
+}
+Functor.prototype.call = function call(that){
+ return this.call.call.apply(this.toFunction(), arguments);
+}
+Functor.prototype.apply = function apply(that, args){
+ return this.apply.apply.apply(this.toFunction(), arguments);
+}
+Functor.prototype.bind = function bind(that){
+ return this.bind.bind.apply(this.toFunction(), arguments);
+}
+
 function Router(matcher, responder){
+ Functor.call(this, this.route);
  if("function" != typeof matcher.match)
   matcher = new Matcher(matcher);
  if("function" != typeof responder)
@@ -6,6 +46,7 @@ function Router(matcher, responder){
  this.matcher = matcher;
  this.respond = responder;
 }
+util.inherits(Router, Functor);
 Router.prototype.route = function route(request){
  if("match" in this.matcher)
   return this.matcher.match(request) ?
@@ -13,18 +54,6 @@ Router.prototype.route = function route(request){
    false;
  if(this.matcher(request))
   return this.respond.bind(this);
-}
-Router.prototype.toFunction = function(){
- return this.route.bind(this);
-}
-Router.prototype.call = function call(that){
- return this.call.call.apply(this.toFunction(), arguments);
-}
-Router.prototype.apply = function apply(that, args){
- return this.apply.apply.apply(this.toFunction(), arguments);
-}
-Router.prototype.bind = function bind(that){
- return this.bind.bind.apply(this.toFunction(), arguments);
 }
 
 function Matcher(predicate){
