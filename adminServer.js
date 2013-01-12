a2e66ecc32faf8429e3a0a50ae00aa6c1096b839
calcnerd256 <calcnerd256@gmail.com>
2013-01-12 01:44:36 +0000

    gconf server in its own file

--- adminServer.js
+++ adminServer.js
@@ -574,59 +574,7 @@ AdminStringServer.prototype.getHttpsRouterList = function getHttpsRouterList(){
     return result;
    }
   )(
-   new MethodRoutingResponder(
-		{
-		    "GET": function(q, s){
-			var u = q.url.split("/");
-			u.shift(); // ""
-			if(!u[u.length - 1]) u.pop(); //remove trailing slash
-			var p = "/" + u.join("/");
-			var kid = child_process.spawn(
-			    "gconftool-2",
-			    ["--all-dirs", p]
-			)
-			var lex = new (formStream.SingleCharacterDelimiterLexerEmitter)(kid.stdout, "\n");
-			lex.on(
-			    "lexer",
-			    function(lineStream){
-				var sliced = new (formStream.FunctionImageStream)(
-				    new (formStream.SlicingStream)(lineStream, 1),
-				    escape
-				);
-				formStream.bufferChunks(
-				    sliced.resume(),
-				    function(p){
-					s.write(
-					    "<a href=\"" +
-						(
-						    function last(xs){
-							return xs[xs.length - 1];
-						    }
-						)(p.split("/")) +
-						"/\">" +
-						p +
-						"</a><br />\n"
-					);
-				    }
-				);
-				lineStream.resume();
-			    }
-			);
-			lex.on(
-			    "end",
-			    function(){
-				var kiddo = child_process.spawn("gconftool-2", ["-a", p]);
-				s.write("<pre>\n");
-				kiddo.stdout.pipe(s);
-				kiddo.on(
-				    "exit",
-				    function(){s.end("</pre>\n");}
-				);
-			    }
-			).resume();
-		    }
-		}
-   )
+   require("./gconf").responder
   )
  );
     return [
