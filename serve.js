ef100842bfb5cee60ac0d17cb379b2769f054a54
calcnerd256 <calcnerd256@gmail.com>
2013-03-03 00:57:31 +0000

    documented startup script a little

--- serve.js
+++ serve.js
@@ -1,6 +1,14 @@
+//import libraries
+//take optional commandline arguments for the ports
+//start the server
+// then start the repl
+//expose the server
+//prolong crashes
+
 var repl = require("repl");
 var fs = require("fs");
 var adminServer = require("./adminServer");
+
 
 var port = 15213;
 var sslPort = 15214;
@@ -13,18 +21,19 @@ if("--sslPort" == process.argv[4])
  if(+process.argv[5] == process.argv[5])
   sslPort = +process.argv[5];
 
+
 this.server = new adminServer.AdminStringServer();
 this.server.init(
-    port,
-    sslPort,
-    {
+ port,
+ sslPort,
+ {
 	key: fs.readFileSync("./certs/ephphatha.key"),
 	cert: fs.readFileSync("./certs/ephphatha.cert")
-    },
-    function(){
+ },
+ function(){
 	console.log(arguments);
 	repl.start(undefined, undefined, undefined, true);
-    }
+ }
 );
 
 global.server = this.server;
