890b1896c5acedf02afbdb9b158119de54b5f12d
Montana Rowe <calcnerd256@gmail.com>
2013-09-14 14:56:01 -0500

    extracted and exposed components of the component that manages child processes

--- processNanny.js
+++ processNanny.js
@@ -5,33 +5,38 @@ var formController = require("./formController");
  var tagShorthand = formController.tagShorthand;
 var child_process = require("child_process");
 
- function createForm(path, fields, process, patch, cls){
-  if(!cls) cls = SimpleFormController;
-  if(!patch) patch = {};
-  var form = new cls();
-  fields.map(function(field){form.fields.push(field);});
-  form.process = process;
-  for(var k in patch)
-   form[k] = patch[k];
-  this.apiState[path] = form;
-  return form;
- }
+var processNanny = this;
 
-    function removeUpToOneTrailingCarriageReturn(s){
-     if(!s.length) return s;
-     if("\r" != s[s.length - 1]) return s;
-     return s.substring(0, s.length - 1);
-    }
+function createForm(path, fields, process, patch, cls){
+ if(!("apiState" in this))
+  this.apiState = {};
 
- function sanitizeHtml(str){
-  return str.split(
-   "&"
-  ).join("&amp;").split(
-   "<"
-  ).join("&lt;").split(
-   "\n"
-  ).join("<br />");
- }
+ if(!cls) cls = SimpleFormController;
+ if(!patch) patch = {};
+ var form = new cls();
+ fields.map(function(field){form.fields.push(field);});
+ form.process = process;
+ for(var k in patch)
+  form[k] = patch[k];
+ this.apiState[path] = form;
+ return form;
+}
+
+function removeUpToOneTrailingCarriageReturn(s){
+ if(!s.length) return s;
+ if("\r" != s[s.length - 1]) return s;
+ return s.substring(0, s.length - 1);
+}
+
+function sanitizeHtml(str){
+ return str.split(
+  "&"
+ ).join("&amp;").split(
+  "<"
+ ).join("&lt;").split(
+  "\n"
+ ).join("<br />");
+}
 
 function ProcessCradle(command, args, options){
  this.kid = child_process.spawn(command, args, options);
@@ -48,116 +53,123 @@ ProcessCradle.prototype.kill = function kill(){
  this.kid.kill();
 };
 
- function helicopterMom(command, args, options){
-  var cradle = new ProcessCradle(command, args, options);
-  this.childProcesses.push(cradle);
-  return this.childProcesses.length - 1;
+function helicopterMom(command, args, options){
+ var cradle = new ProcessCradle(command, args, options);
+ this.childProcesses.push(cradle);
+ return this.childProcesses.length - 1;
+}
+
+function renderKidList(){
+ return [
+  "<ul>",
+  "<li>",
+  this.that.childProcesses.map(
+   function(cradle, i){
+    if(!cradle) return;
+    return i + " " + sanitizeHtml(
+     [
+      cradle.kid,
+      cradle.opticon,
+      [cradle.cmd, cradle["arguments"]],
+      cradle
+     ].join("\n")
+    )
+   }
+  ).filter(
+   function I(x){return x;}
+  ).join("</li>\n<li>\n"),
+  "</li>",
+  "</ul>"
+ ].join("\n")
+}
+
+function handleKill(ob){
+ var i = ob.index;
+ var kids = this.that.childProcesses;
+ if(!(i in kids))
+  return {
+   toHtml: function(){
+    return "index " + i + " out of bounds (TODO: make this an error)";
+   }
+  };
+ var cradle = kids[i];
+ if(!cradle.killed)
+  cradle.kid.kill();
+ delete kids[i]
+ // print proc's stdio
+ return {
+  toHtml: function(){
+   return this.sanitize(this.opticon);
+  },
+  sanitize: sanitizeHtml,
+  opticon: cradle.opticon.join("")
  }
+}
+
+function handleSpawn(ob){
+ var cmd = ob.cmd;
+ var args = ob.args;
+ var i = processNanny.helicopterMom.call(
+  this.that,
+  cmd,
+  (
+   args ? args.split("\n") : []
+  ).map(processNanny.removeUpToOneTrailingCarriageReturn)
+ );
+ return {
+  toHtml: function(){
+   return "child process at index " + i;
+  }
+ };
+}
 
 function init(){
 
-   if(!("apiState" in this))
-    this.apiState = {};
+ this.createForm = createForm;
 
- this.createForm =  createForm;
-
- this.childProcesses = [];
+ if(!this.childProcesses)
+  this.childProcesses = [];
  this.helicopterMom = helicopterMom;
 
- var killList = {
-    toHtml: function(){
-     return [
-      "<ul>",
-      "<li>",
-      this.that.childProcesses.map(
-       function(cradle, i){
-        if(!cradle) return;
-        return i + " " + cradle.sanitizeOutput(
-         [
-          cradle.kid,
-          cradle.opticon,
-          [cradle.cmd, cradle["arguments"]],
-          cradle
-         ].join("\n")
-        )
-       }
-      ).filter(function I(x){return x;}).join("</li>\n<li>\n"),
-      "</li>",
-      "</ul>"
-     ].join("\n")
-    },
-    that: this
-   };
+ var kidList = {
+  toHtml: renderKidList,
+  that: this
+ };
 
-  function handleKill(ob){
-   var i = ob.index;
-   var kids = this.that.childProcesses;
-   if(!(i in kids))
-    return {
-     toHtml: function(){return "index " + i + " out of bounds (TODO: make this an error)";}
-    };
-   var cradle = kids[i];
-   if(!cradle.killed)
-    cradle.kid.kill();
-   delete kids[i]
-   // print proc's stdio
-   return {
-    toHtml: function(){
-     return this.sanitize(this.opticon);
-    },
-    sanitize: sanitizeHtml,
-    opticon: cradle.opticon.join("")
-   }
-  }
+ /*
+ var spawnDump = {
+  toHtml: function(){
+   return this.that.childProcesses.map(
+    function(cradle){
+     var kid = cradle.kid;
+     return [
+      kid.pid,
+      [cradle.cmd, cradle["arguments"]],
+      sanitizeHtml(cradle.opticon.join(""))
+     ].join(" ");
+    }
+   ).join("<br />");
+  },
+  that: this
+ };
+ */
 
  this.createForm(
   "/admin/child/kill/",
   [
    new FormField("index"),//TODO: dropdown of PID, cmdline (truncated?)
-   killList
+   kidList
   ],
   handleKill,
   {that:this}
- )
-
- var spawnDump = {
-    toHtml: function(){
-     return this.that.childProcesses.map(
-      function(cradle){
-       var kid = cradle.kid;
-       return [
-        kid.pid,
-        [cradle.cmd, cradle["arguments"]],
-        sanitizeHtml(cradle.opticon.join(""))
-       ].join(" ");
-      }
-     ).join("<br />");
-    },
-    that: this
-   };
-
-  function handleSpawn(ob){
-   var cmd = ob.cmd;
-   var args = ob.args;
-   var i = this.that.helicopterMom(
-    cmd,
-    (
-     args ? args.split("\n") : []
-    ).map(removeUpToOneTrailingCarriageReturn)
-   );
-   return {
-    toHtml: function(){
-     return "child process at index " + i;
-    }
-   };
-  }
+ );
 
  this.createForm(
   "/admin/spawn/",
   [
    new FormField("cmd"),
    new TextAreaField("args"),
-   spawnDump
+   kidList
   ],
   handleSpawn,
   {that: this}
@@ -165,4 +177,12 @@ function init(){
 
 }
 
+this.createForm = createForm;
+this.removeUpToOneTrailingCarriageReturn = removeUpToOneTrailingCarriageReturn;
+this.sanitizeHtml = sanitizeHtml;
+this.ProcessCradle = ProcessCradle;
+this.helicopterMom = helicopterMom;
+this.renderKidList = renderKidList;
+this.handleKill = handleKill;
+this.handleSpawn = handleSpawn;
 this.init = init;
