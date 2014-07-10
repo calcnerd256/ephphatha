0a0a6f347e2475e0c0755223ea2d6d6dd7dd5a7b
Montana Rowe <calcnerd256@gmail.com>
2014-07-10 16:46:21 -0500

    encapsulate some of the command-line argument-gathering

--- serve.js
+++ serve.js
@@ -11,17 +11,63 @@ var adminServer = require("./adminServer");
 
 
 // ports
+//  get_command_line_arguments
+//  get_ports_from_command_line
+//  actually set the port variables
 
-var port = 15213;
-var sslPort = 15214;
+function get_command_line_arguments(argv){
+ if(arguments.length == 0) argv = process.argv;
 
-if("--port" == process.argv[2])
- if(+process.argv[3] == process.argv[3])
-  sslPort = 1 + (port = +process.argv[3]);
+ var args = {};
+ if("--port" == argv[2])
+  args.port = argv[3];
 
-if("--sslPort" == process.argv[4])
- if(+process.argv[5] == process.argv[5])
-  sslPort = +process.argv[5];
+ if("--sslPort" == argv[4])
+  args.sslPort = argv[5];
+
+ return args;
+}
+
+function get_ports_from_command_line(command_line_arguments, default_port, default_ssl_port){
+
+ switch(arguments.length){
+  case 0:
+   command_line_arguments = get_command_line_arguments(process.argv);
+  case 1:
+   default_port = 15213;
+  case 2:
+   default_ssl_port = 15214
+  default:
+   break;
+ }
+
+ var port = default_port;
+ var sslPort = default_ssl_port;
+
+ var command_line_port = null;
+ if("port" in command_line_arguments)
+  command_line_port = command_line_arguments.port;
+ if("port" in command_line_arguments)
+  if(+command_line_port == command_line_port)
+   sslPort = 1 + (port = +command_line_port);
+
+ var command_line_sslPort = null;
+ if("sslPort" in command_line_arguments)
+  command_line_sslPort = command_line_arguments.sslPort;
+ if("sslPort" in command_line_arguments)
+  if(+command_line_sslPort == command_line_sslPort)
+   sslPort = +command_line_sslPort;
+
+ return {http: port, https: sslPort};
+}
+
+var ports = get_ports_from_command_line(
+ get_command_line_arguments(
+  process.argv
+ )
+);
+var port = ports.http;
+var sslPort = ports.https
 
 
 // start servers
